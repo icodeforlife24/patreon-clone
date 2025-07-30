00485f5ae20fc354b9dc80cdd2a4b5f59328f376
icodeforlife24 <shiviutkarsh@gmail.com>
2025-07-31 01:32:00 +0530

    Refactor layout and add Footer component
    Introduces a new Footer component and updates the layout to include Navbar and Footer. Cleans up global styles by removing custom CSS variables and switches PostCSS and Tailwind configuration to use .mjs files. Removes unused config files and updates dependencies in package.json.

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="bg-black text-white text-center p-4">
+      copyright &copy; {new Date().getFullYear()} Get Me A Coffee. All rights reserved.
+    </div>
+  )
+}
+
+export default Footer
--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Link from 'next/link';
 
-export default function navbar() {
+export default function Navbar() {
   return (
-    <div className="">
-        
+    <>
+    <div className="main bg-black text-white flex justify-between items-center p-4">
+        <h1>Get Me A Coffee</h1>
+        <div className="links flex gap-4">
+           <Link href="/" className="text-white hover:text-gray-300">Home</Link>
+           <Link href="/about" className="text-white hover:text-gray-300">About</Link>
+           <Link href="/contact" className="text-white hover:text-gray-300">Contact Us</Link>
     </div>
+    </div>
+    </>
   )
 }
